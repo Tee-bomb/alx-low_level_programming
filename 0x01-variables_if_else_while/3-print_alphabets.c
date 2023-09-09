fd3e7024e93a0ced308f277e62e9539e46388816
Tee-bomb <thobile.s@icloud.com>
2023-09-08 21:34:58 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        int i;
-        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        for (i = 0; i < 52; i++)
-        {
-                putchar(alp[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
