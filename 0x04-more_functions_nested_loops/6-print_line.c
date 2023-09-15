014d55a0d134813e986f5df804d252e424181601
Tee-bomb <thobile.s@icloud.com>
2023-09-15 15:38:13 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * pritn_line - straight line will be drawn
+ * print_line - straight line will be drawn
  * @n: number of times character is to be printed
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
