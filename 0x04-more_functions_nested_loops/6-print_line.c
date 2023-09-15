31c5fb7e1d35caf08bed381e2a8505123fc37d30
Tee-bomb <thobile.s@icloud.com>
2023-09-15 15:31:33 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * pritn_line - straight line will be drawn
+ * @n: number of times character is to be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
