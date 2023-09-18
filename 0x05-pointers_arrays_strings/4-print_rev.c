300afe39ec19afd15187f76efc659bc57a37025e
Tee-bomb <thobile.s@icloud.com>
2023-09-18 11:18:53 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - print and reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longe = 0;
+	int n;
+
+	while (*s != '\0')
+	{
+		longe++;
+		s++;
+	}
+	s--;
+	while (longe > 0)
+	{
+		putchar(*s);
+		s--;
+		length--;
+	}
+	putchar('\n');
+}
